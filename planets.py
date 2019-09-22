f96bc1d7c9502760fca200fd79b7c6ba384393ec
mansiachuthan <55572620+mansiachuthan@users.noreply.github.com>
2019-09-22 09:54:28 -0700

    Update to planets.py
    added an algorithm to calculate the weights of people on Mars and Jupiter based on their weight on Earth

--- planets.py
+++ planets.py
@@ -1,7 +1,14 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    # write your code here
+    weight = input("What do you weigh on earth?")
+    # calculate weight on mars
+    mars_weight = float(weight) * 0.38
+    # calculate weight on jupiter
+    jupiter_weight = float(weight) * 2.34
+    # print outputs
+    print(" \n On Mars you would weigh " + str(mars_weight) + " pounds.")
+    print(" On Jupiter you would weigh " + str(jupiter_weight) + " pounds.")
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
