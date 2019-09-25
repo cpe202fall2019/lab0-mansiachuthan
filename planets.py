e3eb8ca2cda58fea089400e5c6304ef19bf796d4
mansiachuthan <55572620+mansiachuthan@users.noreply.github.com>
2019-09-25 10:32:42 -0700

    Update planets.py
    deleted extra spaces

--- planets.py
+++ planets.py
@@ -6,8 +6,8 @@ def weight_on_planets():
     # calculate weight on jupiter
     jupiter_weight = float(weight) * 2.34
     # print outputs
-    print(" \n On Mars you would weigh " + str(mars_weight) + " pounds.")
-    print(" On Jupiter you would weigh " + str(jupiter_weight) + " pounds.")
+    print(" \nOn Mars you would weigh " + str(mars_weight) + " pounds.")
+    print("On Jupiter you would weigh " + str(jupiter_weight) + " pounds.")
 
 
 if __name__ == '__main__':
